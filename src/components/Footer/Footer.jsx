2f7e0b6555ad7004543c4ee973a5f7f55bf5a10f
v4ljok <v4ljok.c@gmail.com>
2025-03-13 20:29:58 +0200

    add Header and Footer components with styles; remove unused App.css and index.css

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import styles from './Footer.module.scss';
+
+function Footer(props) {
+
+  return (
+    <footer className={styles.footer}>
+
+    </footer>
+  )
+};
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import styles from './Header.module.scss';
+
+function Header(props) {
+
+  return (
+    <header className={styles.header}>
+
+    </header>
+  )
+};
+
+export default Header;
