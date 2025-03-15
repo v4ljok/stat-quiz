b5bb2c56526594c59a3b214fd0c3dba83f8b88bd
v4ljok <v4ljok.c@gmail.com>
2025-03-15 20:37:46 +0200

    style: enhance Footer component layout and styling; update contact links and improve alignment

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,16 @@
 import styles from './Footer.module.scss';
 
-function Footer(props) {
-
+function Footer() {
   return (
     <footer className={styles.footer}>
       <ul>
-        <li>Kontatid</li>
-        <li>Kontatid</li>
-        <li>Kontatid</li>
-        <li>Kontatid</li>
+        <li><a href="#">Kontaktid</a></li>
+        <li><a href="№">📞 +372 625 9300</a></li>
+        <li><a href="№">✉️ stat@stat.ee</a></li>
+        <li><a href="#">Küpsiste sätted</a></li>
       </ul>
     </footer>
-  )
-};
+  );
+}
 
 export default Footer;
